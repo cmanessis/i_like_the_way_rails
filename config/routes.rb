ad7886e47131e0aebd9561ee14b1b36f5880c929
Joanne Zanetton <joannezanetton@gmail.com>
2015-06-23 10:03:04 +0100

    social buttons added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'homes/show'
+
   devise_for :users
   root to: "thoughts#index"
 
   resources :thoughts
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
