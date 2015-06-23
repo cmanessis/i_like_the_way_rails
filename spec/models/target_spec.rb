8053e120b0b04560d7d1f3c8f7002590c2ba6464
Daryl Watson <darylwtsn@gmail.com>
2015-06-23 16:36:48 +0100

    Hopefully done

--- spec/models/target_spec.rb
+++ spec/models/target_spec.rb
@@ -2,6 +2,4 @@ require 'rails_helper'
 
 describe Target do
 
-  it { is_expected.to have_many :thoughts }
-
 end
--- spec/models/thought_spec.rb
+++ spec/models/thought_spec.rb
@@ -5,6 +5,4 @@ describe Thought do
   it { is_expected.to belong_to :user }
   it { is_expected.to belong_to :target }
 
-  validates :question1, length :{minimum: 3}
-
 end
